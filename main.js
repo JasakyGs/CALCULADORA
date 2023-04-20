3a2bd72245149723c4cce8a8e1293bbb19744e7f
JasakyGs <sebastiangalvis13140@gmail.com>
2023-04-20 14:46:23 -0500

    CalcV2

--- main.js
+++ main.js
@@ -1,41 +1,42 @@
-//variabled
-const num1 = document.getElementById("num1")
-const num2 = document.getElementById("num2")
+const numInput = document.getElementById("num1")
 const operando = document.getElementById("operando")
 const display = document.getElementById("display")
-// const igual = document.getElementById("igual")
-// const suma = document.getElementById("sum")
-const botones = document.querySelector(".calculator")
+let num1Value = ""
 
-//elementos de escucha hacia los botones de (suma.resta.multiplicacion,divicion)
+const botones = document.querySelector(".calculator")
 botones.addEventListener("click" ,(evento)=>{
     if (evento.target.id == "sum"){
         operando.textContent="+"
+        num1Value = numInput.value
+        numInput.value="" 
     }
     if (evento.target.id == "rest"){
         operando.textContent="-"
+        num1Value = numInput.value
+        numInput.value="" 
     }
     if (evento.target.id == "multi"){
         operando.textContent="*"
+        num1Value = numInput.value
+        numInput.value="" 
     }
     if (evento.target.id == "divi"){
         operando.textContent="/"
+        num1Value = numInput.value
+        numInput.value="" 
     }
     if (evento.target.id == "clear"){
         operando.textContent=""
-        operando.textContent = "";
-        num1.value = "";
-        num2.value = "";
+        numInput.value = "";
         display.textContent = "";
+        num1Value = ""
     }
-    
+
     if (evento.target.id == "igual"){
-        const num1Value =+num1.value
-        const num2Value =+num2.value
-    //validacion
+        const num2Value = numInput.value
         if (!isNaN(num1Value) && !isNaN(num2Value)) {
             if(operando.textContent=="+")
-                display.textContent = num1Value + num2Value
+                display.textContent = Number(num1Value) + Number(num2Value)
             if(operando.textContent=="-")
                 display.textContent = num1Value - num2Value
             if(operando.textContent=="*")
